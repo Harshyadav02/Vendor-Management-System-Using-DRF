0816e27d79f99d0b5c03724ba86fca55cec94590
Harshyadav02 <harshyadav5736@gmail.com>
2024-05-15 22:31:30 +0530

    Updated display fields

--- vendor_app/admin.py
+++ vendor_app/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from vendor_app.models import VendorInfo,OrderTracking
+from vendor_app.models import VendorInfo,PurchaseOrder
 
 class AdminVendorInfo(admin.ModelAdmin) :
-    list_display = ['vendor_id','vendor_name','vendor_contact','vendor_address']
+    list_display = ['vendor_code','name','contact_details','address','on_time_delivery_rate','average_response_time','quality_rating_avg','fulfillment_rate']
 
 class AdminOrderTracking(admin.ModelAdmin):
-    list_display=['purchase_order_number','vender_reference','order_date','items_quantity','status',]
+    list_display=['purchase_order_number','vender_reference','order_date','delivery_date','items','items_quantity','quality_rating','issue_date','acknowledgment_date','status']
 
 admin.site.register(VendorInfo,AdminVendorInfo)
-admin.site.register(OrderTracking,AdminOrderTracking)
+admin.site.register(PurchaseOrder,AdminOrderTracking)
