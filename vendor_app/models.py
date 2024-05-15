39a1d3c3abed1eb48fac1a27613cdaff19a9844f
Harshyadav02 <harshyadav5736@gmail.com>
2024-05-15 22:31:51 +0530

    Updated models fields for vendor and order

--- vendor_app/models.py
+++ vendor_app/models.py
@@ -4,12 +4,17 @@ import uuid
 
 class VendorInfo(models.Model):
     
-    vendor_id = models.IntegerField(primary_key=True,)
-    vendor_name=models.CharField(max_length=20,null=False)
-    vendor_contact = models.BigIntegerField(null=False)
-    vendor_address = models.CharField(max_length=200,null=False)
+    vendor_code = models.IntegerField(primary_key=True,)
+    name=models.CharField(max_length=20,null=False)
+    contact_details = models.BigIntegerField(null=False)
+    address = models.CharField(max_length=200,null=False)
+    on_time_delivery_rate=models.FloatField(null=False)
+    average_response_time=models.FloatField(null=False)
+    quality_rating_avg=models.FloatField(null=False)
+    fulfillment_rate=models.FloatField(null=False)
 
-class OrderTracking(models.Model) :
+class PurchaseOrder(models.Model) :
+    
     STATUS_CHOICES =(
 
         ('Accepted', 'Accepted'),
@@ -19,8 +24,14 @@ class OrderTracking(models.Model) :
         ('Cancel', 'Cancel'),
         ('Pending', 'Pending'),
     )
+
     purchase_order_number=models.CharField(primary_key=True,max_length=200,default=uuid.uuid4)
-    vender_reference=models.ForeignKey(VendorInfo,on_delete=models.CASCADE,null=False,to_field='vendor_id')
-    order_date=models.DateField(null=False)
+    vender_reference=models.ForeignKey(VendorInfo,on_delete=models.CASCADE,null=False,to_field='vendor_code')
+    order_date=models.DateTimeField(null=False)
+    delivery_date=models.DateTimeField(null=False)
+    items=models.JSONField()
     items_quantity=models.IntegerField(null=False)
+    quality_rating=models.IntegerField(null=True)
+    issue_date=models.DateTimeField(null=False)
+    acknowledgment_date=models.DateField(null=False)
     status=models.CharField(choices=STATUS_CHOICES,max_length=100,null=False)
